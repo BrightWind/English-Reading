4bb91ddf4b84d4043322f6a4dca338aa7f309c42
Mark Tao <653158302@qq.com>
2018-11-01 23:35:33 +0800

    add draft view page

---
+++ client/reader/src/components/Reference.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <label>this is reference page</label>
+    <div class="document-list">
+      <li v-for="doc in docs">
+        <div class="document-item">
+          <label>doc.title</label>
+        </div>
+      </li>
+    </div>
+    <div>
+      <footer></footer>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Reference',
+  data () {
+    return {
+      docs: [{title: 'doc1'}, {title: 'doc2'}]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
--- client/reader/src/router/index.js
+++ client/reader/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Reference from '@/components/Reference'
+import Document from '@/components/Document'
 
 Vue.use(Router)
 
@@ -8,6 +10,16 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Reference',
+      component: Reference
+    },
+    {
+      path: '/Document',
+      name: 'Document',
+      component: Document
+    },
+    {
+      path: '/HelloWorld',
       name: 'HelloWorld',
       component: HelloWorld
     }
