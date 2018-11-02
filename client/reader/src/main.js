8a14206060fae5c85078b5313b5a7cda85284ab5
Mark Tao <653158302@qq.com>
2018-11-03 00:54:25 +0800

    add some sample

--- client/reader/src/main.js
+++ client/reader/src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
+import VueX from 'vuex'
 import App from './App'
 import router from './router'
-
+import store from './store'
 Vue.config.productionTip = false
+Vue.use(VueX)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
